b90f065ec54ced1ba5e5970943acfcd45f03719d
Sulaimanovuran <sulaimanovuran@gmail.com>
2022-06-10 18:28:40 +0600

    the game

---
+++ some_text.py
@@ -0,0 +1,8 @@
+board = range(1,10)
+def draw_board():
+    print('-------------')
+    for i in range(3):
+        print('|', board[0+i*3], '|', board[1+i*3], '|', board[2+i*3], '|')
+        print('-------------')
+
+
